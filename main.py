11cc95b4bebcede7a270f3dd732b2efe0e5cb829
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-01-01 21:46:08 +0900

    Mulitiple Parameters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def say_hello(user_name):
+def say_hello(user_name, user_age):
     print("Hello", user_name, "How are you?")
+    print("you are", user_age, "years old")
 
-say_hello("Son MinSeok")
+say_hello("Son MinSeok", 26)
