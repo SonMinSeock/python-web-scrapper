c1a48885328855b079cc4d6406d817c930522950
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-01-01 21:59:26 +0900

    덧셈, 뺄셈, 곱셈, 나눗셈, 제곱 계산기 함수 만들어보기

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-def say_hello(user_name="anonymous"):
-    print("Hello", user_name, "How are you?")
+def plus(first_num = 0, second_num = 0):
+    result = first_num + second_num
+    print(first_num, "+", second_num, "=", result)
 
-say_hello("Son MinSeok")
-say_hello()
+def minus(first_num=0, second_num=0):
+    result = first_num - second_num
+    print(first_num, "-", second_num, "=", result)
+
+def multiply(first_num=0, second_num=0):
+    result = first_num * second_num
+    print(first_num, "X", second_num, "=", result)
+
+def divide(first_num=0, second_num=0):
+    result = first_num / second_num
+    print(first_num, "/", second_num, "=", result)
+
+def powerOf(first_num=0, second_num=0):
+    result = first_num ** second_num
+    print(first_num, "^", second_num, "=", result)
+
+plus(2, 3)
+minus(3, 7)
+multiply(9, 5)
+divide(6, 3)
+powerOf(2, 8)
