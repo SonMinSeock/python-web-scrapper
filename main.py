6c9d325ade227c30b95260d138363fbbdfa6bb9b
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-01-02 10:52:46 +0900

    while

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from random import randint, random
+"""
+from random import randint
 
 user_choice = int(input("Choose number:"))
 pc_choice = randint(1, 50)
@@ -9,3 +10,11 @@ elif user_choice > pc_choice:
     print("Lower! Computer choice ", pc_choice)
 elif user_choice < pc_choice:
     print("Higher! Computer choice ", pc_choice)
+"""
+
+# while 구문
+distance = 0
+
+while distance < 20:
+    print(f"I'm running: {distance}km" )
+    distance += 1
