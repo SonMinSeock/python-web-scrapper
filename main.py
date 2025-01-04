a454dd89268411de538475d1e34411f8e94b4ec7
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-01-04 21:05:27 +0900

    For문

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-player = {
-    "name": "Son",
-    "age": 26,
-    "alive": True,
-    "fav_food": ["🍕", "🍔"]
-}
+websites = (
+    "goggle.com",
+    "airbnb.com",
+    "twitter.com",
+    "facebook.com"
+) 
 
-print(player.get("age"))
-# print(player.get("fav_food"))
-print(player["fav_food"])
-
-player["xp"] = 1500
-print(player)
-
-player["fav_food"].append("🍜")
-print(player["fav_food"])
+for website in websites:
+    print(website)
