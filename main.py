aac6642b8bd6d33789bec6529ea308908e7689d5
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-01-04 21:41:09 +0900

    Status Codes

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from requests import get
 websites = (
-    "goggle.com",
+    "google.com",
     "https://airbnb.com",
     "twitter.com",
     "facebook.com",
     "https://tiktok.com"
 ) 
 
+results = {}
+
 for website in websites:
-    if not website.startswith("https://") or not website.startswith("http://"):
+    if not website.startswith("https://"):
         website = f"https://{website}"
-    print(website)
+    response = get(website)
+    
+    if response.status_code == 200:
+        results[website] = "OK"        
+    else:
+        results[website] = "FAILED"
+
+print(results)
