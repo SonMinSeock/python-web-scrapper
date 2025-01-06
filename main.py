c432c62a1c7d48cf801d8b5b5f2025a06cab6c25
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-01-06 20:21:00 +0900

    Classes

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
-player = {}
+class Puppy:
+    def __init__(self):
+        print("Puppy is born!")
+        self.name = "뚜비"
+        self.age = 0.1
+        self.breed = "비숑"
+        
 
-def create_player(name, xp, team_name):
-    return {
-    "name": name,
-    "XP": xp,
-    "team": team_name
-}
+puppy = Puppy()
 
-def introduce_player(player):
-    name = player["name"]
-    team = player["team"]
-    print(f"Hello! My name is {name} and I play for {team}")
-
-son = create_player("Son", 1000, "Team X")
-nico = create_player("Nico", 1500, "Team Blue")
-
-teams = {
-    "Team X": [son],
-    "Team Blue": [nico]
-}
-
-print(teams)
+print(puppy.name, puppy.breed, puppy.age)
