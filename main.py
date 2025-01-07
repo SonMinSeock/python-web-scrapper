a195442d6ad34da04874f7b1e0cfdaa01e08e1a6
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-01-07 16:35:36 +0900

    Code Challange

--- main.py
+++ main.py
@@ -1,44 +1,68 @@
-class Dog:
-    def __init__(self, name, breed, age):
+class Player:
+    def __init__(self, name, team):
         self.name = name
-        self.age = age
-        self.breed = breed
-    def sleep(self):
-        print(f"{self.name} zzzzzz...")
+        self.xp = 1500
+        self.team = team
+
+    def introduce(self):
+        print(f"Hello! I'm {self.name} and I play for {self.team}")
+
+
+class Team:
+    def __init__(self, team_name):
+        self.team_name = team_name
+        self.players = []
+
+    def add_player(self, name):
+        new_player = Player(name, self.team_name)
+        self.players.append(new_player)
+
+    def delete_player(self, name):
+        is_delete = False
+
+        for player in self.players:
+            if player.name == name:
+                self.players.remove(player)
+                print(f"{self.team_name}, {player.name}님 나갔습니다.")
+                is_delete = not is_delete
+                break
+                
+        if not is_delete:
+            print(f"{name}님 {self.team_name}에 존재하지 않습니다.")
+
+    def show_players(self):
+        for player in self.players:
+            player.introduce()
     
-class GuardDog(Dog):
-    def __init__(self, name, breed):
-        super().__init__(name, breed, 5)
-        self.aggresive = True
+    def show_xp(self):
+        sum_xp = 0
 
-    def rrrr(self):
-        print("stay away!")
-    
-    
-class Puppy(Dog):
-    def __init__(self, name, breed):
-        super().__init__(name, breed, 0.1)
-        self.spoiled = True
-    def __str__(self):
-        return f"Puppy named {self.name}, breed: {self.breed}"
-    def woof_woof(self):
-        print("Woof Woof!")
+        for player in self.players:
+            sum_xp += player.xp
 
-puppy1 = Puppy(name="뚜비", breed="비숑")
-puppy2 = Puppy(name="톰", breed="요크셔테리어")
-puppy3 = Puppy(name="콩", breed="요크셔테리어")
-puppy4 = Puppy(name="담비", breed="말티즈")
+        print(f"{self.team_name}의 총 XP는 {sum_xp}입니다.")
 
-guard_dog1 = GuardDog(name="그리즈", breed="달마시안")
 
-print(puppy1.name, puppy1.breed, puppy1.age)
-print(puppy2.name, puppy2.breed, puppy2.age)
-print(puppy3.name, puppy3.breed, puppy3.age)
-print(puppy4.name, puppy4.breed, puppy4.age)
+team_red = Team("Team Red")
+team_blue = Team("Team Blue")
 
-print(puppy1, puppy2, puppy3, puppy4)
+team_red.add_player("Son")
+team_red.add_player("Kim")
+team_red.add_player("Lee")
 
-puppy1.woof_woof()
-guard_dog1.rrrr()
-puppy1.sleep()
-guard_dog1.sleep()
+team_blue.add_player("Nico")
+team_blue.add_player("Lynn")
+team_blue.add_player("Kue")
+
+
+team_red.show_players()
+team_blue.show_players()
+
+team_red.delete_player("Kim")
+team_red.show_players()
+
+team_red.show_xp()
+team_blue.show_xp()
+
+team_red.show_xp()
+team_blue.show_xp()
