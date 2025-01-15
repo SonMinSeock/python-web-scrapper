f61eda71f1f145bc100dec43f1701cabb6a27208
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-01-15 11:00:25 +0900

    Jobs

--- main.py
+++ main.py
@@ -6,5 +6,14 @@ url = "https://weworkremotely.com/remote-full-time-jobs"
 reponse = requests.get(url)
 
 soup = BeautifulSoup(reponse.content, "html.parser")
-jobs = soup.find("section", class_="jobs").find_all("li")
-print(jobs)
+jobs = soup.find("section", class_="jobs").find_all("li")[:-1]
+
+for job in jobs:
+    title = job.find("span", class_="title").text
+
+    company, position, region = job.find_all("span", class_="company")
+    region = region.text
+    company = company.text
+    position = position.text
+
+    print(title, region, company, position, "-----\n")
