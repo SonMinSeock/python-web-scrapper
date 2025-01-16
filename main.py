4e9b6e036e2e3d817af99bb6cb5e1fc177e84763
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-01-16 11:26:18 +0900

    Pagination

--- main.py
+++ main.py
@@ -1,28 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://weworkremotely.com/remote-full-time-jobs"
-
-reponse = requests.get(url)
-
-soup = BeautifulSoup(reponse.content, "html.parser")
-jobs = soup.find("section", class_="jobs").find_all("li")[:-1]
-
 all_jobs = []
 
-for job in jobs:
-    title = job.find("span", class_="title").text
-    company, position, region = job.find_all("span", class_="company")
-    
-    url = job.find("div", class_="tooltip--flag-logo").next_sibling["href"]
+def get_pages(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    return len(soup.find("div", class_="pagination").find_all("span", class_="page"))
 
-    job_data = {
-        "title": title,
-        "company": company.text,
-        "position": position.text,
-        "region": region.text,
-        "url": f"https://weworkremotely.com{url}"
-    }
+def scrape_page(url):
+    print(f"Scrapping {url}...")
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    jobs = soup.find("section", class_="jobs").find_all("li")[:-1]
 
-    all_jobs.append(job_data)
-print(all_jobs)
+    for job in jobs:
+        title = job.find("span", class_="title").text
+        company, position, region = job.find_all("span", class_="company")
+        
+        url = job.find("div", class_="tooltip--flag-logo").next_sibling["href"]
+
+        job_data = {
+            "title": title,
+            "company": company.text,
+            "position": position.text,
+            "region": region.text,
+            "url": f"https://weworkremotely.com{url}"
+        }
+
+        all_jobs.append(job_data)
+
+
+total_pages = get_pages("https://weworkremotely.com/remote-full-time-jobs?page=1")
+
+for page in range(total_pages):
+    url = f"https://weworkremotely.com/remote-full-time-jobs?page={page+1}"
+    scrape_page(url)
+
+print(len(all_jobs))
