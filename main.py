ec9a9653d4d06ff3e1f71b026190b6f341c677af
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-01-20 10:45:37 +0900

    Keyword Arguments

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from playwright.sync_api import sync_playwright
+# from playwright.sync_api import sync_playwright
 
-playwright = sync_playwright().start()
+# playwright = sync_playwright().start()
 
-browser = playwright.chromium.launch(headless=False)
+# browser = playwright.chromium.launch(headless=False)
 
-page = browser.new_page()
+# page = browser.new_page()
 
-page.goto("https://google.com")
+# page.goto("https://google.com")
 
-page.screenshot(path="screenshot.png")
+# page.screenshot(path="screenshot.png")
+
+def plus(a, b, c, d, e):
+    return a + b
+
+plus(10, d=20, a=False, c=[1, 2, 3 ,4, 5], e="hello")
