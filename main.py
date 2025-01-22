78fb0e89c5297da9a55e3189c8d9f3aad838821f
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-01-22 21:51:01 +0900

    Collecting Jobs 코드 작성중

--- main.py
+++ main.py
@@ -39,3 +39,26 @@ content = page.content()
 playwright.stop()
 
 soup = BeautifulSoup(content, "html.parser")
+
+jobs = soup.find_all("div", class_="JobCard_container__REty8")
+
+jobs_db = []
+
+for job in jobs:
+    link = f"https://www.wanted.co.kr/{job.find('a')['href']}"
+    title = job.find("strong", class_="JobCard_title__HBpZf").text
+    company_name = job.find("span", class_="JobCard_companyName__N1YrF").text
+    reward = job.find("span", class_="JobCard_reward__cNlG5").text
+
+    job = {
+        "title": title,
+        "link": link,
+        "company_name": company_name,
+        "reward": reward
+    }
+
+    jobs_db.append(job)
+
+
+print(jobs_db)
+print(len(jobs_db))
