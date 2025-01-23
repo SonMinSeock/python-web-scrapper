318782df175d41a947d78688092188f8a1489092
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-01-23 14:23:57 +0900

    계산기 코드수정

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # BLUEPRINT | DONT EDIT
 playing = True
 
-a = int(input("Choose a number:\n"))
-b = int(input("Choose another one:\n"))
+a = input("Choose a number:\n")
+b = input("Choose another one:\n")
 operation = input(
     "Choose an operation:\n    Options are: + , - , * or /.\n    Write 'exit' to finish.\n"
 )
