da018d4ced583fd496067dffa26df055348cc0b2
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-01-28 20:44:13 +0900

    코드챌린지 - berlinstartupjobs.com 웹사이트용 스크래퍼를 만들기

--- main.py
+++ main.py
@@ -1,65 +1,110 @@
 # BLUEPRINT | DONT EDIT
-import requests
 
-movie_ids = [
-    238, 680, 550, 185, 641, 515042, 152532, 120467, 872585, 906126, 840430
-]
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get(
+    "https://berlinstartupjobs.com/engineering/",
+    headers={
+        "User-Agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    })
+
+skills = ["python", "typescript", "javascript", "rust"]
 
 # /BLUEPRINT
 
 # 👇🏻 YOUR CODE 👇🏻:
 
-def getMovie(url, movie_id):
-    try:
-        response = requests.get(f"{url}/movies/{movie_id}", timeout=10)
-        # response.raise_for_status()  # HTTP 상태 코드 확인, 커스텀 상태 코드 메시지 출력한다고 하면 이부분 제외하면 된다.
-
-    except requests.exceptions.Timeout:
-        print(f"Request timed out for movie ID {movie_id}")
-        return
-    except requests.exceptions.ConnectionError:
-        print(f"Connection error for movie ID {movie_id}")
-        return
-    except requests.exceptions.HTTPError as http_err:
-        print(f"HTTP error for movie ID {movie_id}: {http_err}")
-        return
-    except requests.exceptions.RequestException as req_err:
-        print(f"Request failed for movie ID {movie_id}: {req_err}")
-        return
-    
-    # 커스텀 상태 코드 메시지
-    if 500 <= response.status_code < 600:
-        print("Server Error")
-        return
-    elif 400 <= response.status_code < 500:
-        print("Client Error")
-        return
-    elif 300 <= response.status_code < 400:
-        print("Redirection Messages")
-        return
-
-    try:
-        data = response.json()
-    except ValueError: # ValueError는 주로 값 자체가 적합하지 않을 때 처리한다. ex) JSON 파싱 실패, 함수 호출 시 잘못된 타입이나 범위의 값을 전달했을 때 경우.
-        print("Invalid JSON response")
-        return
-    return data
-
-    
-
-url = "https://nomad-movies.nomadcoders.workers.dev"
-
-for movie_id in movie_ids:
-    movie = getMovie(url, movie_id)
-
-    if movie:
-        print(f"Movie ID: {movie_id}")
-        print(f"Title: {movie['title']}")
-        print(f"Overview: {movie['overview']}")
-        print(f"Vote Average: {round(movie['vote_average'], 2)}")
-        print("=====================================")
-    else:
-        print(f"No movie data for Movie ID {movie_id}")
-        print("=====================================")
-
 # /YOUR CODE
+job_enginer_db = []
+job_db = {}
+
+def getPages(page_content):
+    if not page_content:
+        print("URL 페이지네이션 할 URL이 없습니다!")
+        return
+    else:
+        soup = BeautifulSoup(page_content, "html.parser")
+        pages = soup.select(".page-numbers")
+        return len(pages) - 1
+
+pages = getPages(response.content)
+
+def scraper_enginer(url, page):
+    # https://berlinstartupjobs.com/engineering/page/2 -> 두 번째 페이지
+    response = requests.get(
+        f"{url}/engineering/page/{page}",
+        headers={
+            "User-Agent":
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+        })
+    
+    soup = BeautifulSoup(response.content, "html.parser")
+    jobs = soup.find("ul", class_="jobs-list-items").find_all("li", class_="bjs-jlid")
+    
+    for job in jobs:
+        title = job.find("h4", class_="bjs-jlid__h").text
+        job_type = job.find("a", class_="bjs-jlid__b").text
+        link = job.find("a", class_="bjs-jlid__b")["href"]
+        
+        decription = job.find("div", class_="bjs-jlid__description").text
+       
+        job_data = {
+            "title": title,
+            "job_type": job_type,
+            "decription": decription,
+            "link": link
+        }
+
+        job_enginer_db.append(job_data)
+
+def scraper_job(url, keyword):
+    response = requests.get(
+        f"{url}/skill-areas/{keyword}",
+        headers={
+            "User-Agent":
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+        })
+    
+    soup = BeautifulSoup(response.content, "html.parser")
+    
+    jobs = soup.find("ul", class_="jobs-list-items").find_all("div", class_="bjs-jlid__wrapper")
+
+    for job in jobs:
+        company_name_element = job.find("a", class_="bjs-jlid__b")
+        
+        company_name = company_name_element.text
+        job_title = job.find("h4", class_="bjs-jlid__h").text
+        link = company_name_element["href"]
+        decription = job.find("div", class_="bjs-jlid__description").text
+    
+       
+        job_data = {
+            "company_name": company_name,
+            "title": job_title,
+            "decription": decription,
+            "link": link
+        }
+
+        job_db[keyword].append(job_data)
+
+
+
+        
+for page in range(pages):
+    scraper_enginer("https://berlinstartupjobs.com", page + 1)
+
+print(f"Pagination 후 엔지니어 일자리 스크래핑 한 데이터")
+print("================================")
+print(job_enginer_db)
+
+for skill in skills:
+    print(f"{skill}에 대한 일자리 스크래핑....")
+    job_db[skill] = []
+    scraper_job("https://berlinstartupjobs.com", skill)
+
+for skill in skills:
+    print(f"{skill}에 대한 일자리:")
+    print(job_db[skill])
+    print("================================")
