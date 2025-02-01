250c116dd6f5d9ed6bbbcfc2560cf677ba1edd0c
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-02-01 18:16:23 +0900

    Hello Flask

---
+++ extractors/wanted.py
@@ -0,0 +1,68 @@
+from playwright.sync_api import sync_playwright
+from bs4 import BeautifulSoup
+import csv
+import time
+
+class JobScraper:
+    def __init__(self, keyword):
+        self.keyword = keyword
+    
+    def get_page_content(self, url):
+        playwright = sync_playwright().start()
+
+        browser = playwright.chromium.launch(headless=False)
+
+        page = browser.new_page()
+
+        page.goto(url)
+
+        time.sleep(5)
+
+        content = page.content()
+
+        playwright.stop()
+
+        return content
+
+    def scraper_jobs(self, content):
+        soup = BeautifulSoup(content, "html.parser")
+
+        jobs = soup.find_all("div", class_="JobCard_container__REty8")
+
+        jobs_db = []
+
+        for job in jobs:
+            link = f"https://www.wanted.co.kr/{job.find('a')['href']}"
+            title = job.find("strong", class_="JobCard_title__HBpZf").text
+            company_name = job.find("span", class_="JobCard_companyName__N1YrF").text
+            reward = job.find("span", class_="JobCard_reward__cNlG5").text
+
+            job = {
+                "title": title,
+                "link": link,
+                "company_name": company_name,
+                "reward": reward
+            }
+
+            jobs_db.append(job)
+    
+        return jobs_db
+
+    def write_csv(self, keyword, jobs_db):
+        file = open(f"{keyword}_jobs.csv", "w")
+
+        writter = csv.writer(file)
+        writter.writerow(jobs_db[0].keys())
+
+        for job in jobs_db:
+            writter.writerow(job.values())
+        
+        file.close()
+
+    def run(self):
+        
+        content = self.get_page_content(f"https://www.wanted.co.kr/search?query={self.keyword}&tab=overview")
+
+        jobs_db = self.scraper_jobs(content)
+
+        self.write_csv(self.keyword, jobs_db)
--- main.py
+++ main.py
@@ -1,68 +1,9 @@
-class Player:
-    def __init__(self, name, team):
-        self.name = name
-        self.xp = 1500
-        self.team = team
+from flask import Flask
 
-    def introduce(self):
-        print(f"Hello! I'm {self.name} and I play for {self.team}")
+app = Flask("JobScrapper")
 
+@app.route("/")
+def home():
+    return "Hello Flask!"
 
-class Team:
-    def __init__(self, team_name):
-        self.team_name = team_name
-        self.players = []
-
-    def add_player(self, name):
-        new_player = Player(name, self.team_name)
-        self.players.append(new_player)
-
-    def delete_player(self, name):
-        is_delete = False
-
-        for player in self.players:
-            if player.name == name:
-                self.players.remove(player)
-                print(f"{self.team_name}, {player.name}님 나갔습니다.")
-                is_delete = not is_delete
-                break
-                
-        if not is_delete:
-            print(f"{name}님 {self.team_name}에 존재하지 않습니다.")
-
-    def show_players(self):
-        for player in self.players:
-            player.introduce()
-    
-    def show_xp(self):
-        sum_xp = 0
-
-        for player in self.players:
-            sum_xp += player.xp
-
-        print(f"{self.team_name}의 총 XP는 {sum_xp}입니다.")
-
-
-team_red = Team("Team Red")
-team_blue = Team("Team Blue")
-
-team_red.add_player("Son")
-team_red.add_player("Kim")
-team_red.add_player("Lee")
-
-team_blue.add_player("Nico")
-team_blue.add_player("Lynn")
-team_blue.add_player("Kue")
-
-
-team_red.show_players()
-team_blue.show_players()
-
-team_red.delete_player("Kim")
-team_red.show_players()
-
-team_red.show_xp()
-team_blue.show_xp()
-
-team_red.show_xp()
-team_blue.show_xp()
+app.run("0.0.0.0", port=4500, debug=True)
